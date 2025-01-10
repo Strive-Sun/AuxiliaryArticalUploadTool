f5ee67d2c68057e94cfe407f79a86b5f21b61d51
sunjinlei <sunjinlei@bilibili.com>
2025-01-10 16:16:27 +0800

    feat: 更新应用程序名称和显示信息

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,15 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-    MainWindow window;
-    window.show();
-    return app.exec();
+    QApplication a(argc, argv);
+    
+    // 设置应用程序信息
+    QApplication::setApplicationName("文章编辑上传工具");
+    QApplication::setApplicationDisplayName("文章编辑上传工具");
+    QApplication::setOrganizationName("AuxiliaryArticalUploadTool");
+    QApplication::setOrganizationDomain("com.example.auxiliaryarticaluploadtool");
+    
+    MainWindow w;
+    w.show();
+    return a.exec();
 } 
